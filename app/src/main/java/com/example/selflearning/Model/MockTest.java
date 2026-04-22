a4b630c902f270b80afb7886ae4702af1f43121f
Pankaj <pk9278424@gmail.com>
2026-04-22 10:46:33 +0530

    Create `MockTest` model class
    - Implement `MockTest` in the `Model` package with a `test` string field.
    - Add a constructor and a getter method for the `test` property.

---
+++ app/src/main/java/com/example/selflearning/Model/MockTest.java
@@ -0,0 +1,13 @@
+package com.example.selflearning.Model;
+
+public class MockTest {
+    private String test;
+
+    public MockTest(String test) {
+        this.test = test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+}
