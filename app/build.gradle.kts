820d9f9b91dd52cd7c82afb708d2c09bd66b9552
Pankaj <pk9278424@gmail.com>
2026-04-26 19:02:33 +0530

    Add commented-out `defaultConfig` block in `app/build.gradle.kts`
    - Add a duplicate, commented-out version of the `defaultConfig` configuration block for reference.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,16 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+//    defaultConfig {
+//        applicationId = "com.example.selflearning1"
+//        minSdk = 27
+//        targetSdk = 36
+//        versionCode = 1
+//        versionName = "1.0"
+//
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
