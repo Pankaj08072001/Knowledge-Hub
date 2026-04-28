01d29e6afa45eff50be43b00a51d8cdc3bd83c4e
Pankaj <pk9278424@gmail.com>
2026-04-28 22:05:54 +0530

    Override `onCreateAnimation` in `JavaFragment` and clean up `build.gradle.kts`
    - Implement the `onCreateAnimation` method in `JavaFragment` to call the superclass implementation.
    - Remove a redundant empty comment line in `app/build.gradle.kts`.

--- app/src/main/java/com/example/selflearning/Fragment/JavaFragment.java
+++ app/src/main/java/com/example/selflearning/Fragment/JavaFragment.java
@@ -1,7 +1,14 @@
 package com.example.selflearning.Fragment;
 
+import android.view.animation.Animation;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class JavaFragment extends Fragment {
-
+    @Nullable
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        return super.onCreateAnimation(transit, enter, nextAnim);
+    }
 }
